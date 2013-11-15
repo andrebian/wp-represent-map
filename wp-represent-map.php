27ea783c7ecfed353073a1c85ef774a63cb6cfe3
Andre Cardoso <andrecardosodev@gmail.com>
2013-11-15 17:59:21 -0200

    Iniciado plugin

---
+++ wp-represent-map.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP Represent Map
+Plugin URI: 
+Description: Possibilita o cadastro e manutenção de diversoso tipos de itens em mapas.
+Version: 1.0.0
+Author: Andre Cardoso aka andrebian
+Author URI: http://andrebian.com
+License: GPLv2
+*/
+
+
