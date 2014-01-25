1f9cf0d729ba797bc8ecaef2a8f602768d5481aa
Andre Cardoso <andrecardosodev@gmail.com>
2014-01-25 15:08:30 -0200

    Changed @since from 1.0.0 to 0.1, the current version and applied filter on post in address

--- includes/admin/ajax-actions.php
+++ includes/admin/ajax-actions.php
@@ -2,20 +2,22 @@
 /**
  * Groups all ajax actions from WP Represent Map
  * 
- * @since 1.0.0
+ * @since 0.1
  */
 
 
 /**
  * Takes the latitude and logintude from an address
  * 
- * @since 1.0.0
+ * @since 0.1
  */
 function get_lat_lng()
 {
     $url_vendor_map = 'http://maps.googleapis.com/maps/api/geocode/json';
     
-    $address_details = json_decode(file_get_contents($url_vendor_map . '?address=' . urlencode($_POST['address']) . '&sensor=false'), true);
+    $address = filter_input(INPUT_POST, 'address', FILTER_SANITIZE_STRING);
+    
+    $address_details = json_decode(file_get_contents($url_vendor_map . '?address=' . urlencode($address) . '&sensor=false'), true);
     $lat_lng_prepare = $address_details['results'][0]['geometry']['location'];
     
     $lat_lng = implode(',', $lat_lng_prepare);
